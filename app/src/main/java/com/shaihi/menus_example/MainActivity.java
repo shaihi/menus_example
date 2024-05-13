60b4f8ba3fd7e7901e3a6f2a465340ed8ca17e50
shaihi <shaihi@icloud.com>
2024-05-13 12:46:43 +0300

    Initial commit

---
+++ app/src/main/java/com/shaihi/menus_example/MainActivity.java
@@ -0,0 +1,18 @@
+package com.shaihi.menus_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
