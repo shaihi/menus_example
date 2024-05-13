532847bc8442fef151c17c8d3e1591fbb7531f5a
shaihi <shaihi@icloud.com>
2024-05-13 14:18:08 +0300

    finished adding some menu items and inflated the menu in main activity

--- app/src/main/java/com/shaihi/menus_example/MainActivity.java
+++ app/src/main/java/com/shaihi/menus_example/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
+        inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 }
