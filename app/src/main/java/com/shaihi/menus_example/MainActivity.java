6560bac33a0db296c990171be470d1cac74747b0
shaihi <shaihi@icloud.com>
2024-06-17 12:50:23 +0300

    Added another menu layout for the context menu. It is the same layout, but in different file for ease of understanding and to change according to your needs. Added the code to MainActivity to register the "View" to listen to ContextMenu interaction (means it will appear for a long press on the View). Added the code to handle the selection of a menu item.

--- app/src/main/java/com/shaihi/menus_example/MainActivity.java
+++ app/src/main/java/com/shaihi/menus_example/MainActivity.java
@@ -3,11 +3,16 @@ package com.shaihi.menus_example;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv = findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.sharp_brightness_7_24);
+
+        CardView cv = findViewById(R.id.cardView); //Remember CardView "is a" View
+        registerForContextMenu(cv);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -45,5 +56,29 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+
+    //Floating Context Menu implementation
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT)
+                .show();
+        if (item.getItemId() == R.id.firstContextTest) {
+            return true;
+        } else if (item.getItemId() == R.id.secondContextTest) {
+            return true;
+        } else if (item.getItemId() == R.id.thirdContextTest) {
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
